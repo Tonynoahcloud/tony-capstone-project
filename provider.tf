e161af404d91e45831ab5f1c7ec5fa4959c34087
Noah Anthony <tonynoah37@gmail.com>
2024-08-15 11:04:47 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -23,8 +23,4 @@ terraform {
 
 provider "azurerm" {
   features {}
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
-  subscription_id = var.subscription_id
 }
